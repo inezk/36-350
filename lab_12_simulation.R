2a0bbe95689abf4ff37ed10e1cb7b824e856e3c8
inezk <inezk@andrew.cmu.edu>
2018-04-17 20:37:54 -0400

    2c

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -9,9 +9,28 @@ generate_data = function(n, p){
 model_select = function(covariates, responses, cutoff){
   reg = lm(responses ~ covariates)
   pval = summary(reg)$coefficients[,4]
-  retain = covariates[which(pval <= cutoff),]
-  res_ret = response[which(pval <= cutoff)]
-  regnew = lm(res_ret ~ retain)
-  newpval = summary(regnew)$coefficients[,4]
+  retain = covariates[which(pval <= cutoff)]
+  res_ret = responses[which(pval <= cutoff)]
+  if(length(retain) >  0){
+    regnew = lm(res_ret ~ retain)
+    newpval = summary(regnew)$coefficients[,4]
+  }
+  else{
+    return(vector())
+  }
+  if(all(is.na(x))) {
+    return(vector())}
   return(newpval)
 }
+
+run_simulation = function(n_trials, n, p, cutoff){
+  pval_list = c()
+  for(i in 1:n_trials){
+    results = generate_data(n, p)
+    pval = model_select(results$covariates, results$responses, cutoff)
+    pval_list = c(pval_list, pval)
+  }
+  hist(pval_list)
+}
+
+run_simulation(10, n = 100, p = 10, 0.05)
